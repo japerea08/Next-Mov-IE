01375fd1d207ad66f81a35660d6af5c0a3c13c0a
Nelson Diaz <abnerdc86@gmail.com>
2018-03-23 10:51:10 -0400

    adding persistentData.js

---
+++ assets/js/persistentData.js
@@ -0,0 +1,13 @@
+//Data persistency
+//This code will add a user object to the database with the
+//properties: uid, favorites, zip
+var userData = {
+	 
+}
+
+
+
+$(document).ready(function() {
+	console.log('this is the current user', sessionStorage.user);
+	
+})
