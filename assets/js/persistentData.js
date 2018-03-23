35e9fa9617729679f8268dcb9e3aee36cb777189
Nelson Diaz <abnerdc86@gmail.com>
2018-03-23 16:15:23 -0400

    working on data persistency. not yet finished

--- assets/js/persistentData.js
+++ assets/js/persistentData.js
@@ -1,13 +1,16 @@
 //Data persistency
-//This code will add a user object to the database with the
-//properties: uid, favorites, zip
-var userData = {
-	 
+
+//This method will receive a user id, a list of genres and a zip code
+//and it will update the information for that use in the database
+function updateUserSetting(uid, pref, zip){
+  var user = {
+    favorites: favorites,
+    zip: zip
+  };
+  database.ref('users/' + uid).set(user);
 }
 
-
-
 $(document).ready(function() {
-	console.log('this is the current user', sessionStorage.user);
+  
 	
 })
