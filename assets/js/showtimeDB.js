5933fd85a35938ca24d6e5d6ac9755140bf91b8d
Joey Perea <j.perea08@gmail.com>
2018-03-24 09:30:31 -0400

    new api key

--- assets/js/showtimeDB.js
+++ assets/js/showtimeDB.js
@@ -1,5 +1,5 @@
-//var showTimeApi = "6uzdzbe7uny7nc23edk38r4d";
-var showTimeApi = "4brme8bk3hts4dcngvgyjeyh";
+var showTimeApi = "6uzdzbe7uny7nc23edk38r4d";
+//var showTimeApi = "4brme8bk3hts4dcngvgyjeyh";
 //theater object to help build movie objects
 
 var theaterMap = {};
